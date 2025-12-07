a58d7cdb2d7d16c9223be2124cedbb8196e93ba2
Leo <leoh0987@gmail.com>
2025-12-07 15:20:32 -0600

    Initial DB Schema

---
+++ database/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    friends TEXT,
+    pending_friend_requests TEXT
+);
+
+CREATE TABLE closet(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    clothing_type VARCHAR(100),
+    color VARCHAR(100),
+    material VARCHAR(100),
+    style VARCHAR(100),
+    description TEXT,
+    img_filename VARCHAR(255) NOT NULL,
+    public BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
