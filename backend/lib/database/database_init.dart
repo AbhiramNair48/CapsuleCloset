d4272b9d3ed7225789cbb8abe0556edff41613d4
Leo <leoh0987@gmail.com>
2025-12-09 20:37:05 -0600

    Revert "New Sql Schema"
    This reverts commit 49c3a9a68366909e4847d7dafbee37f9bdc0baac.

--- backend/lib/database/database_init.dart
+++ backend/lib/database/database_init.dart
@@ -3,7 +3,13 @@ import 'package:mysql_client/mysql_client.dart';
 // import 'package:http/http.dart' as http;
 import 'dart:io';
 
+
+
 // TODO: change db password to .env variable
+
+
+
+
 Future<void> main() async {
 
   // final response = await http.get(Uri.parse('https://api.ipify.org?format=json'));
@@ -14,11 +20,12 @@ Future<void> main() async {
   //   print('Failed to get public IP');
   // }
 
-  final db_ip = "127.0.0.1"; 
+
+  final ip = "104.190.141.175"; 
   final db_port = 3306;
   // Create a connection settings object
-  final pool = MySQLConnectionPool(
-    host: db_ip,
+  final conn = MySQLConnectionPool(
+    host: ip,
     port: db_port,
     userName: "root",
     password: "root",
@@ -27,30 +34,16 @@ Future<void> main() async {
   );
 
     final server = await HttpServer.bind(
-    InternetAddress.anyIPv4, // NOTE: change to InternetAddress.anyIPv4 or ur ip address
+    InternetAddress.anyIPv4, // NOTE: change to InternetAddress.loopvackIPv4 or ur ip address
     8080,
   );
 
   print('Server running on http://${server.address.address}:${server.port}/');
   await for (HttpRequest request in server) {
-    if (request.method == 'POST' && request.uri.path == '/signup') {
-      var username = request.uri.queryParameters['username'];
-      var password = request.uri.queryParameters['password'];
-
-      var signupStatement = await pool.prepare('INSERT INTO users (username, password_hash, )')
-        
-     
-     }
-    if (request.method == 'POST' && request.uri.path == '/login') {
-     
-     
-     }
-    // request.response
-    //   ..headers.contentType = ContentType.text
-    //   ..write('Hello')
-    //   ..close();
+    request.response
+      ..headers.contentType = ContentType.text
+      ..write('Test from Dart HTTP server')
+      ..close();
   }
 
 }
-
-
