11c100fc5c739669ba96b2cb4f1ca81093aef2de
Leo <leoh0987@gmail.com>
2025-12-09 19:20:03 -0600

    Server Hosting

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -1,7 +1,17 @@
-// import 'dart:convert';
-// import 'package:shelf/shelf.dart';
-// import 'package:shelf/shelf_io.dart' as io;
-// import 'package:shelf_router/shelf_router.dart';
+import 'dart:io';
 
-// import 'package:capsule_closet_app/database/database_init.dart';
+Future<void> main() async{
+    final server = await HttpServer.bind(
+    InternetAddress.loopbackIPv4,
+    8080,
+  );
 
+  print('Server running on http://${server.address.address}:${server.port}/');
+  await for (HttpRequest request in server) {
+    request.response
+      ..headers.contentType = ContentType.html
+      ..write('<h1>Hello from Dart HTTP server!</h1>')
+      ..close();
+  }
+
+}
